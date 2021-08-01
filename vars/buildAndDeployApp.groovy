623416d9dea3f451b4d4d58ed156fa7f0fb760f5
sourabhgupta385 <gupta.sourabh385@gmail.com>
2021-08-01 11:37:18 +0530

    adding docker image build and publish image

--- vars/buildAndDeployApp.groovy
+++ vars/buildAndDeployApp.groovy
@@ -3,26 +3,39 @@ def call(String agentLabel) {
     pipeline {
         agent none
         stages {
-            stage('Unit Test') {
-                agent { label agentLabel }
-                steps {
-                    // git branch: 'main', url: ''
-                    sh "npm install"
-                    sh "npm run unit-test"
-                    stash includes: 'coverage/*', name: 'coverage-report' 
-                }
-            }
+            // stage('Unit Test') {
+            //     agent { label agentLabel }
+            //     steps {
+            //         // git branch: 'main', url: ''
+            //         sh "npm install"
+            //         sh "npm run unit-test"
+            //         stash includes: 'coverage/*', name: 'coverage-report' 
+            //     }
+            // }
 
-            stage('Code Quality Analysis') {
+            // stage('Code Quality Analysis') {
+            //     agent {
+            //         kubernetes {
+            //             yamlFile 'k8s-manifests/slaves/sonar-scanner-slave.yaml'
+            //         }
+            //     }
+            //     steps {
+            //         container('sonar-scanner') {
+            //             unstash 'coverage-report'
+            //             sh "sonar-scanner -Dsonar.qualitygate.wait=true"
+            //         }
+            //     }
+            // }
+
+            stage('Build & Publish Docker Image') {
                 agent {
                     kubernetes {
-                        yamlFile 'k8s-manifests/sonar-scanner-pod.yaml'
+                        yamlFile 'k8s-manifests/slaves/kaniko-slave.yaml'
                     }
                 }
                 steps {
-                    container('sonar-scanner') {
-                        unstash 'coverage-report'
-                        sh "sonar-scanner -Dsonar.qualitygate.wait=true"
+                    container('kaniko') {
+                        sh "/kaniko/executor -f `pwd`/Dockerfile -c `pwd` --destination=nodejs-ci-cd:${BUILD_NUMBER}"
                     }
                 }
             }
