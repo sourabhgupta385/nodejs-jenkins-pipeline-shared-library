2e76011557c534596739c5a0d6818fcaa1099f95
sourabhgupta385 <gupta.sourabh385@gmail.com>
2021-08-05 17:14:43 +0530

    publishing report

--- vars/buildAndDeployApp.groovy
+++ vars/buildAndDeployApp.groovy
@@ -33,13 +33,16 @@ def call(String agentLabel) {
                         yamlFile 'k8s-manifests/slaves/owasp-dependency-check-slave.yaml'
                     }
                 }
+                
                 steps {
                     container('owasp-dependency-checker') {
-                        sh "pwd"
+                        sh "/usr/share/dependency-check/bin/dependency-check.sh --project 'DNVA' --scan ./package.json --format ALL"
                         sh "ls -ltr"
+                        sh "cd target && ls -ltr"
                     }
                 }
-                // dependencyCheckPublisher pattern: 'target/dependency-check-report.xml'
+                
+                dependencyCheckPublisher pattern: 'target/dependency-check-report.xml'
             }
 
             // stage('Build & Publish Docker Image') {
