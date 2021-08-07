d0a8b0652188a585bf013f22078567c0213e2e80
sourabhgupta385 <gupta.sourabh385@gmail.com>
2021-08-07 18:06:33 +0530

    publishing report

--- vars/buildAndDeployApp.groovy
+++ vars/buildAndDeployApp.groovy
@@ -97,7 +97,15 @@ def call(String agentLabel) {
                         unstash 'docker-image'
                         sh "mkdir -p /tmp/trivy"
                         sh "chmod 754 /tmp/trivy"
-                        sh "trivy image --cache-dir /tmp/trivy --input dvna_devsecops_${BUILD_NUMBER}.tar"
+                        sh "trivy --cache-dir /tmp/trivy image --format template --template '@contrib/html.tpl' -o trivy-report.html --input dvna_devsecops_${BUILD_NUMBER}.tar"
+                        publishHTML target: [
+                            allowMissing: false,
+                            alwaysLinkToLastBuild: true,
+                            keepAll: true,
+                            reportDir: './',
+                            reportFiles: 'trivy-report.html',
+                            reportName: 'Docker Imange Scan Report'
+                        ]                    
                     }
                 }
             }
