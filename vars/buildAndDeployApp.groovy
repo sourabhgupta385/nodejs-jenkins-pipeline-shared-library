ccf0c59e89d4a049b826fac2e24b6e74efd7fdeb
sourabhgupta385 <gupta.sourabh385@gmail.com>
2021-08-08 17:32:30 +0530

    added json format reports

--- vars/buildAndDeployApp.groovy
+++ vars/buildAndDeployApp.groovy
@@ -3,56 +3,57 @@ def call(String agentLabel) {
     pipeline {
         agent none
         stages {
-            // stage('Unit Test') {
-            //     agent { label agentLabel }
+            stage('Unit Test') {
+                agent { label agentLabel }
 
-            //     steps {
-            //         sh "npm install"
-            //         sh "npm run unit-test"
-            //         stash includes: 'coverage/*', name: 'coverage-report' 
-            //         stash includes: 'node_modules/', name: 'node_modules' 
-            //     }
-            // }
+                steps {
+                    sh "npm install"
+                    sh "npm run unit-test"
+                    stash includes: 'coverage/*', name: 'coverage-report' 
+                    stash includes: 'node_modules/', name: 'node_modules' 
+                }
+            }
 
-            // stage('Static Application Security Testing') {
-            //     agent {
-            //         kubernetes {
-            //             yamlFile 'k8s-manifests/slaves/nodejsscan-slave.yaml'
-            //         }
-            //     }
+            stage('Static Application Security Testing') {
+                agent {
+                    kubernetes {
+                        yamlFile 'k8s-manifests/slaves/nodejsscan-slave.yaml'
+                    }
+                }
                 
-            //     steps {
-            //         container('nodejsscanner') {
-            //             sh "njsscan src --html -o 'nodejs-scanner-report.html' || true"
-            //             sh "ls -ltr"
-            //             publishHTML target: [
-            //                 allowMissing: false,
-            //                 alwaysLinkToLastBuild: true,
-            //                 keepAll: true,
-            //                 reportDir: './',
-            //                 reportFiles: 'nodejs-scanner-report.html',
-            //                 reportName: 'SAST Report'
-            //             ]
-            //         }
-            //     }
-            // }
+                steps {
+                    container('nodejsscanner') {
+                        sh "njsscan src --html -o 'nodejs-scanner-report.html' --json -o 'nodejs-scanner-report.json' || true"
+                        sh "ls -ltr"
+                        publishHTML target: [
+                            allowMissing: false,
+                            alwaysLinkToLastBuild: true,
+                            keepAll: true,
+                            reportDir: './',
+                            reportFiles: 'nodejs-scanner-report.html',
+                            reportName: 'SAST Report'
+                        ]
+                        stash includes: 'nodejs-scanner-report.json', name: 'nodejs-scanner-report' 
+                    }
+                }
+            }
 
-            // stage('Software Composition Analysis') {
-            //     agent {
-            //         kubernetes {
-            //             yamlFile 'k8s-manifests/slaves/owasp-dependency-check-slave.yaml'
-            //         }
-            //     }
+            stage('Software Composition Analysis') {
+                agent {
+                    kubernetes {
+                        yamlFile 'k8s-manifests/slaves/owasp-dependency-check-slave.yaml'
+                    }
+                }
                 
-            //     steps {
-            //         container('owasp-dependency-checker') {
-            //             unstash 'node_modules'
-            //             sh "/usr/share/dependency-check/bin/dependency-check.sh --project 'DNVA' --scan ./package.json --format ALL"
-            //             dependencyCheckPublisher pattern: "dependency-check-report.xml"
-            //             stash includes: "dependency-check-report.xml,dependency-check-report.json,dependency-check-report.html", name: 'owasp-reports' 
-            //         }
-            //     }
-            // }
+                steps {
+                    container('owasp-dependency-checker') {
+                        unstash 'node_modules'
+                        sh "/usr/share/dependency-check/bin/dependency-check.sh --project 'DNVA' --scan ./package.json --format ALL"
+                        dependencyCheckPublisher pattern: "dependency-check-report.xml"
+                        stash includes: "dependency-check-report.xml,dependency-check-report.json,dependency-check-report.html", name: 'owasp-reports' 
+                    }
+                }
+            }
 
             // stage('Code Quality Analysis') {
             //     agent {
@@ -97,15 +98,17 @@ def call(String agentLabel) {
                         unstash 'docker-image'
                         sh "mkdir -p /tmp/trivy"
                         sh "chmod 754 /tmp/trivy"
-                        sh script: 'trivy --cache-dir /tmp/trivy image --format template --template "@contrib/html.tpl" -o trivy-report.html --input dvna_devsecops_${BUILD_NUMBER}.tar'
-                        publishHTML target: [
-                            allowMissing: false,
-                            alwaysLinkToLastBuild: true,
-                            keepAll: true,
-                            reportDir: './',
-                            reportFiles: 'trivy-report.html',
-                            reportName: 'Docker Imange Scan Report'
-                        ]                    
+                        sh script: 'trivy --cache-dir /tmp/trivy image --format json -o trivy-report.json --input dvna_devsecops_${BUILD_NUMBER}.tar'
+                        // publishHTML target: [
+                        //     allowMissing: false,
+                        //     alwaysLinkToLastBuild: true,
+                        //     keepAll: true,
+                        //     reportDir: './',
+                        //     reportFiles: 'trivy-report.html',
+                        //     reportName: 'Docker Imange Scan Report'
+                        // ] 
+                        sh "ls -ltr"   
+                        stash includes: 'trivy-report.json', name: 'trivy-report'                 
                     }
                 }
             }
