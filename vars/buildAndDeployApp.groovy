6a6dcdfb806ddf8ec423f30c0f63b2c4b89f7cee
sourabhgupta385 <gupta.sourabh385@gmail.com>
2021-08-17 14:09:42 +0530

    adding functional testing stage

--- vars/buildAndDeployApp.groovy
+++ vars/buildAndDeployApp.groovy
@@ -177,8 +177,7 @@ def call() {
                 }
                 steps {
                     container('newman') {
-                        sh "sed -i 's/APP_STAGING_TARGET_URL/${properties.APP_STAGING_TARGET_URL}/g' ${properties.POSTMAN_COLLECTION_FILE_PATH}"
-                        sh "newman run ${properties.POSTMAN_COLLECTION_FILE_PATH} --reporters junit --reporter-junit-export='newman-report.xml'"
+                        sh "newman run ${properties.POSTMAN_COLLECTION_FILE_PATH} --global-var 'api_url=${properties.APP_STAGING_TARGET_URL}' --reporters junit --reporter-junit-export='newman-report.xml'"
                         junit 'newman-report.xml'
                     }
                 }
