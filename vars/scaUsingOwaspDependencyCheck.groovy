5e2771d3c4865e5eebf75329ec82387cf3c9811a
sourabhgupta385 <gupta.sourabh385@gmail.com>
2021-08-19 18:35:51 +0530

    creating seperate groovy files for each tool

--- vars/scaUsingOwaspDependencyCheck.groovy
+++ vars/scaUsingOwaspDependencyCheck.groovy
@@ -1,4 +1,4 @@
-def call() {
-    sh "/usr/share/dependency-check/bin/dependency-check.sh --project 'DNVA' --scan ${properties.PACKAGE_JSON_PATH} --format ALL"
+def call(String projectName, String packageJsonPath) {
+    sh "/usr/share/dependency-check/bin/dependency-check.sh --project ${projectName} --scan ${packageJsonPath} --format ALL"
     dependencyCheckPublisher pattern: "dependency-check-report.xml" 
 }
